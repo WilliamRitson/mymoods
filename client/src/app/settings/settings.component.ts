2ee4ed75e99e4b352cb823a9bb8d86803b84f181
William Ritson <william.ritson@gmail.com>
2019-02-02 12:44:04 -0800

    Added export capabilites.
    You can now export data as CSV or JSON.

--- client/src/app/settings/settings.component.ts
+++ client/src/app/settings/settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MoodStorageService } from '../mood-storage.service';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  constructor(private moodStorage: MoodStorageService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  public exportAsJson() {
+    const data = JSON.stringify(this.moodStorage.getMoodValues());
+    const blob = new Blob([data], { type: 'application/json;charset=utf-8' });
+    FileSaver.saveAs(blob, 'data.json');
   }
 
+  public exportAsCSV() {
+    const data = `Date, Mood
+      ${this.moodStorage.getMoodValues().map(moodValue => moodValue.time + ', ' + moodValue.mood).join('\n')}`;
+    const blob = new Blob([data], { type: 'text/csv;charset=utf-8' });
+    FileSaver.saveAs(blob, 'data.csv');
+  }
 }
