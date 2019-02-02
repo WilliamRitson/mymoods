ec1d9e285a04df119f26bcc4e3f1d7e05ab1be80
William Ritson <william.ritson@gmail.com>
2019-02-01 23:05:09 -0800

    added angular material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoodEntryComponent } from './mood-entry/mood-entry.component';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
